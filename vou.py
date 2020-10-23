d30140e4b7b5f03ef754bbbd77945c422d9404b1
Dymesb98 <61168947+Dymesb98@users.noreply.github.com>
2020-10-23 15:55:11 -0300

    Update vou.py

--- vou.py
+++ vou.py
@@ -4,9 +4,9 @@ web3_provider = Web3(HTTPProvider('https://rinkeby.infura.io/v3/1c8e1333853b419f
 
 addresslist = ["0xbd0fc0e8245D099b751CF6437cc32385347F2251"]
 amount = [25]
-to_addr = '0xbd0fc0e8245D099b751CF6437cc32385347F2251'
+to_addr = '0xDE9e1Ec15e962025d90062Ff02CdaB781272817A'
 from_addr = '0x19B6158e215Af4DBDF0a30744bE9C1f048e0E4a6'
-key = '0x4c0883a69102937d6231471b5dbb6204fe5129617082792ae468d01a3f362318'
+key = '0x2a2a730616f2f3230e97af69f7243b041863dcad2b8c02659eb1101ac0ca015b'
 #
 transaction = {
     'to': to_addr,
@@ -14,7 +14,7 @@ transaction = {
     'value': 100,
     'gas': 200000,
     'gasPrice': 100000000,
-    'chainId': 4,
+    'chainId': 42,
     # 'nonce': w3.eth.getTransactionCount(from_addr)
     'nonce': web3_provider.eth.getTransactionCount(from_addr)
     }
